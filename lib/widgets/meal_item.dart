0f40ace218f0bf3af606e6012df944083f9b6741
tuanit168 <tuanit168@gmail.com>
2021-11-07 22:43:43 +1100

    Complete displaying meal list details

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,38 @@ class MealItem extends StatelessWidget {
     required this.duration,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+  
   void selectMeal() {}
 
   @override
@@ -45,7 +77,51 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Icon(Icons.schedule,),
+                    SizedBox(width: 6,),
+                    Text('$duration min'),
+                  ],),
+                  Row(children: <Widget>[
+                    Icon(Icons.work,),
+                    SizedBox(width: 6,),
+                    Text(complexityText),
+                  ],),
+                    Row(children: <Widget>[
+                    Icon(Icons.attach_money,),
+                    SizedBox(width: 6,),
+                    Text(affordabilityText),
+                  ],),
+                ],
+              ),
             ),
           ],
         ),
