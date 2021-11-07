8960a54c71f73447a0ea5f4ffaa7c7ef6c9330fe
tuanit168 <tuanit168@gmail.com>
2021-11-07 23:47:01 +1100

    Use case for onGenerateRoute() & onUnknownRoute()

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,10 @@ class MyApp extends StatelessWidget {
         ),
         textTheme: const TextTheme(
           bodyText1: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
+            color: Color.fromRGBO(20, 51, 51, 1),
           ),
           bodyText2: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
+            color: Color.fromRGBO(20, 51, 51, 1),
           ),
           headline6: TextStyle(
             fontSize: 20,
@@ -44,6 +44,19 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        // if (settings.name == '/meal-detail') {
+        //   return ...;
+        // }
+        // else if (settings.name == '/something-else') {
+        //   return ...;
+        // }
+        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
       },
     );
   }
